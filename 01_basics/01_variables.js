dd59702994d80febd2f380415775153b9d336f05
Raj <126488198+rajhowladar@users.noreply.github.com>
2024-09-03 06:04:20 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 2234 
+let accountEmail = "rajhowladar@mail.com"
+var accountPassword = " 12345"
+accountCity = "raipur"
+
+// acountId= 22222  not allowed because of const 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all js code as newer version
+
+// alert (3+3 ) we are using nodejs , not browser 
+
+
+console.log(3+3); 
+// use separate line avoid using same line, readability  prettier
+console.log("raj");
+
+// java has very less datatypes
+
+let name = "  raj "
+let age = 18
+let isLoggedIn = false
+
+//number 
+//bigint
+//string => " "
+//boolean => true false
+//null => standalone value
+// undefined =>  let state;
+// symbol => unique
+
+// these are the primitive datatypes
+
+// number ==> range  2 to power 53  and above that are bigINT
+
+
+//object
+
+// console.log(typeof null); => object
+// console.log(typeof undefined); => undefined
