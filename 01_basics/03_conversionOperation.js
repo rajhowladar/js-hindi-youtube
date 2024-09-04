09957573538121c2d6f77070500f23fab2f1a9fc
Raj <126488198+rajhowladar@users.noreply.github.com>
2024-09-04 13:14:46 +0000

    updated conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -52,7 +52,45 @@
 //                        for any kind of number or string value is true or false for empty
 //                         string or 0 value
 
-let loggedIn = 1  // true for 1        // false for empty string " "
+// let loggedIn = 1    // true for 1        // false for empty string " "
 
-let booleanValue = Boolean(loggedIn)
-console.log(booleanValue);
+// let booleanValue = Boolean(loggedIn)
+// console.log(booleanValue);
+
+
+//***********************OPERATIONS **********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+// let str1 = "hello "
+// let str2 = " Raj"
+// console.log(str1+str2);
+
+// console.log("1"+2);       // = 12
+// console.log(1+"2");      // = 12
+// console.log("1"+2+2); // output = 122
+// console.log(1+2+"2");  // output= 32
+// these type of questions are only competitive approach 
+// code must be readable use () for better under standing
+
+// console.log(true) // output = true
+// console.log(+true) // output = 1
+// console.log(+"");      // output = 0
+
+// let num1 , num2, num3
+//  num1 = num2 = num3 = 2+2
+
+// let gameCounter = 100
+// gameCounter++;
+// console.log(gameCounter);
+
+
