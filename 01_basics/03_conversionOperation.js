bcb3b2acc7c7c5676d108260c252397545876589
Raj <126488198+rajhowladar@users.noreply.github.com>
2024-09-04 12:53:37 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,23 +1,58 @@
+//  for value like 33abc     the value inside valueNumber is NaN
+
+// let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// -----------------------for value of null        the value inside is 0
+
+// let score = null
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+//------------------------- for value = udefined  the value inside is NaN
+
+// let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+//-------------------for boolean value = true the value inside is 1 for false 0
+
 // let score = true
 
 // console.log(typeof score);
 // console.log(typeof(score));
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber);
-// console.log(valueInNumber);
+// let valueNumber = Number(score)
 
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
 
-// "33"  --> 33
-// "33abc"  -->  NaN
-// true  -->  1
+// -----------------------for boolean type conversion of integer number 1
+//                        for any kind of number or string value is true or false for empty
+//                         string or 0 value
 
-// let isLoggedIn =1
+let loggedIn = 1  // true for 1        // false for empty string " "
 
-// let booleanIsLoggedIn = Boolean(isLoggedIn)
-// console.log(booleanIsLoggedIn);
-
-let somenumber= 33
-let stringNumber= String(somenumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+let booleanValue = Boolean(loggedIn)
+console.log(booleanValue);
