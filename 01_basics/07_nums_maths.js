be20c9520b25fcd4beee8856cba7ebe9a22c7cb4
Raj <126488198+rajhowladar@users.noreply.github.com>
2024-09-06 07:19:45 +0000

    nums and maths

---
+++ 01_basics/07_nums_maths.js
@@ -0,0 +1,14 @@
+const score = 400
+
+// we can explicitly define variable as a particular Number datatype variable
+// for ex :
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length); // now we can perform string operation in number
+console.log(balance.toFixed(2));
+
+
+const otherNumber = 23.84555
