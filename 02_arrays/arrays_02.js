ee44d4c07fcbadc218fc1a4a2c363be6d8c5e973
Raj <126488198+rajhowladar@users.noreply.github.com>
2024-09-07 09:21:19 +0000

    array 2

---
+++ 02_arrays/arrays_02.js
@@ -0,0 +1,13 @@
+const marvel_heros = ["thor", "Ironman","spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros);
+
+// console.log(marvel_heros[3][1]);
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros);
